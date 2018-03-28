25e9d30ebf0f2784a1d169195d87dde58f8d04b6
x-xira25-x <jenny.hofmann@he-arc.ch>
2018-03-28 08:55:40 +0200

    Revert "création entité visite avec relation"
    This reverts commit c9e60ebb2870dd17ee5faf68bf2f5e1224feae87.

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -3,7 +3,6 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ProjetJHipster2H2ClientModule } from './client/client.module';
 import { ProjetJHipster2H2VendeurModule } from './vendeur/vendeur.module';
 import { ProjetJHipster2H2BienModule } from './bien/bien.module';
-import { ProjetJHipster2H2VisiteModule } from './visite/visite.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
@@ -11,7 +10,6 @@ import { ProjetJHipster2H2VisiteModule } from './visite/visite.module';
         ProjetJHipster2H2ClientModule,
         ProjetJHipster2H2VendeurModule,
         ProjetJHipster2H2BienModule,
-        ProjetJHipster2H2VisiteModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
