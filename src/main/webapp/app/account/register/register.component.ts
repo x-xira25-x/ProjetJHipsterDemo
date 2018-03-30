5b8d1dc1522823eec7a06d57858164158e60f770
x-xira25-x <jenny.hofmann@he-arc.ch>
2018-03-30 16:09:43 +0200

    ajout du client lors de l'inscription/ne fonctionne pas

--- src/main/webapp/app/account/register/register.component.ts
+++ src/main/webapp/app/account/register/register.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit, AfterViewInit, Renderer, ElementRef } from '@angular/core';
-import { HttpErrorResponse } from '@angular/common/http';
+import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 import { Register } from './register.service';
 import { LoginModalService, EMAIL_ALREADY_USED_TYPE, LOGIN_ALREADY_USED_TYPE } from '../../shared';
+import { ClientService} from '../../entities/client/client.service';
+import {Client} from '../../entities/client/client.model';
+import {UserService} from "../../shared/user/user.service";
+import {User} from "../../shared/user/user.model";
 
 @Component({
     selector: 'jhi-register',
@@ -19,18 +23,25 @@ export class RegisterComponent implements OnInit, AfterViewInit {
     registerAccount: any;
     success: boolean;
     modalRef: NgbModalRef;
+    client: Client;
+
 
     constructor(
         private loginModalService: LoginModalService,
         private registerService: Register,
         private elementRef: ElementRef,
-        private renderer: Renderer
+        private renderer: Renderer,
+        private clientService : ClientService,
+        private userService : UserService,
     ) {
     }
 
     ngOnInit() {
         this.success = false;
         this.registerAccount = {};
+        this.client = {};
+
+
     }
 
     ngAfterViewInit() {
@@ -46,10 +57,26 @@ export class RegisterComponent implements OnInit, AfterViewInit {
             this.errorUserExists = null;
             this.errorEmailExists = null;
             this.registerAccount.langKey = 'fr';
+
+
+
             this.registerService.save(this.registerAccount).subscribe(() => {
                 this.success = true;
+                //retourne le user
+                this.userService.find(this.registerAccount.login).subscribe(resp=>{
+                    console.log('essai '+ resp.body.login);
+                    this.client.user =resp.body;
+                    console.info('login client :' +this.client.user.login);
+                    // appeler la sauvegarde du client
+
+                    console.info('appel create client');
+                    this.clientService.create(this.client)
+                });
+
+
             }, (response) => this.processError(response));
-        }
+
+                    }
     }
 
     openLogin() {
