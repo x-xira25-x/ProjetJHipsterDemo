b72c4df2db924b4a7010b62bcea723b5338320c9
x-xira25-x <jenny.hofmann@he-arc.ch>
2018-03-30 20:59:47 +0200

    page visiteClient montre toutes les visites

--- src/main/webapp/app/visite-client/visite-client.component.ts
+++ src/main/webapp/app/visite-client/visite-client.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Visite} from "../entities/visite/visite.model";
+import {VisiteService} from "../entities/visite/visite.service";
+import {HttpErrorResponse, HttpResponse} from "@angular/common/http";
+import {JhiAlertService} from "ng-jhipster";
 
 @Component({
   selector: 'jhi-visite-client',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class VisiteClientComponent implements OnInit {
+    visites: Visite[];
+  constructor(
+      private visiteService: VisiteService,
+      private jhiAlertService: JhiAlertService
+  ) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    loadAll() {
+        this.visiteService.query().subscribe(
+            (res: HttpResponse<Visite[]>) => {
+                this.visites = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
+    ngOnInit() {
+        this.loadAll();
+      //  this.principal.identity().then((account) => {
+           // this.currentAccount = account;
+       // });
+       // this.registerChangeInVisites();
+    }
+    private onError(error) {
+        this.jhiAlertService.error(error.message, null, null);
+    }
 
 }
