6c3b324f4b0882810171513e052faab32d8571e4
x-xira25-x <jenny.hofmann@he-arc.ch>
2018-04-01 20:41:01 +0200

    ajout bouton se désister

--- src/main/webapp/app/visite-client/visite-client.component.ts
+++ src/main/webapp/app/visite-client/visite-client.component.ts
@@ -2,11 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import {Visite} from '../entities/visite/visite.model';
 import {VisiteService} from '../entities/visite/visite.service';
 import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
-import {JhiAlertService} from 'ng-jhipster';
+import {JhiAlertService, JhiEventManager} from 'ng-jhipster';
 import {AccountService} from '../shared/auth/account.service';
 import {SessionsComponent} from '../account/sessions/sessions.component';
 import {Account} from '../shared/user/account.model';
 import {Principal} from '../shared/auth/principal.service';
+import {Client} from "../entities/client/client.model";
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {VendeurService} from "../entities/vendeur/vendeur.service";
+import {BienService} from "../entities/bien/bien.service";
+import {ClientService} from "../entities/client/client.service";
+import {Vendeur} from "../entities/vendeur/vendeur.model";
+import {Bien} from "../entities/bien/bien.model";
 
 @Component({
   selector: 'jhi-visite-client',
@@ -18,13 +25,25 @@ export class VisiteClientComponent implements OnInit {
     login:String;
     //account: any;
     settingsAccount: any;
+    visite: Visite;
+    isSaving: boolean;
+    client: Client;
+    vendeurs: Vendeur[];
+    biens: Bien[];
+
+    clients: Client[];
 
   constructor(
       private visiteService: VisiteService,
       private jhiAlertService: JhiAlertService,
-
       private account: AccountService,
-      private principal: Principal
+      private principal: Principal,
+    public activeModal: NgbActiveModal,
+    private vendeurService: VendeurService,
+    private bienService: BienService,
+    private clientService: ClientService,
+    private eventManager: JhiEventManager,
+
 
   ) { }
 
@@ -49,6 +68,41 @@ export class VisiteClientComponent implements OnInit {
             this.currentAccount = account;
         });*/
        // this.registerChangeInVisites();
+    }
+
+// essaie suppression client à la visite
+    desister(idVisite) {
+        console.log('entre dans la inscripton');
+        // récupérer  client
+        this.principal.identity().then((account) => {
+            this.settingsAccount = this.copyAccount(account);
+            this.bienService.findIdClient(this.settingsAccount.login).subscribe(
+                (res: HttpResponse<Client>) => {
+                    this.client = res.body;
+                    console.log('client' + this.client.id);
+                    // essayer de récupérer la visite et mettre le client dedans
+                    this.visiteService.find(idVisite).subscribe(
+                        (res: HttpResponse<Visite>) => {
+                            this.visite=res.body;
+                            //  console.log('visite'+ this.visite.id);
+                            this.visite.client= null;
+                            //   console.log('ajout client'+ this.visite.client.id);
+                            this.visiteService.updateSansConvert(this.visite).subscribe(
+                                (res: HttpResponse<Visite>) => {
+                                    this.visite = res.body;
+                                    console.log('update visite');
+                                },
+                                (res: HttpErrorResponse) => this.onError(res.message)
+                            );
+                        });
+                    /*   this.bienService.ajoutClientVisite(idVisite,this.client.id).subscribe(
+                           (res: HttpResponse<Visite>) => {
+                               this.visite = res.body;
+                           },
+                           (res: HttpErrorResponse) => this.onError(res.message)
+                       );*/
+                });
+        });
     }
     private onError(error) {
         this.jhiAlertService.error(error.message, null, null);
